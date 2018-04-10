e3b441a3e48be64b5e756bc9eecf5af4e1aa29d6
maiquelknechtel <maiquelknechtel@10.0.0.100>
2018-04-09 22:47:02 -0300

    retirando import

--- src/main/java/br/com/nutricao/JpaController/ProfissionalSaudeJpaController.java
+++ src/main/java/br/com/nutricao/JpaController/ProfissionalSaudeJpaController.java
@@ -7,7 +7,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import br.com.nutricao.bean.Paciente;
 import br.com.nutricao.bean.ProfissionalSaude;
 
 @Stateless
