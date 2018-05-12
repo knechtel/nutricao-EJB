feb99335afc473f8cf82932d4f6246be798ccb63
Lucas Vaccaro <lucas-vaccaro@outlook.com>
2018-05-12 18:27:25 -0300

    Modificado escopo e corrigidos redirects

--- src/main/java/br/com/nutricao/controller/PacienteBean.java
+++ src/main/java/br/com/nutricao/controller/PacienteBean.java
@@ -7,25 +7,25 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import br.com.nutricao.JpaController.ExameJpaControllerRemote;
 import br.com.nutricao.JpaController.PacienteJpaControllerRemote;
 import br.com.nutricao.bean.Exame;
 import br.com.nutricao.bean.Paciente;
-@SessionScoped
-@Named("pacienteBean")
 
+@RequestScoped
+@Named("pacienteBean")
 public class PacienteBean implements Serializable {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
+	
 	@EJB
 	private PacienteJpaControllerRemote pacienteController;
+	
 	@EJB
 	private PacienteJpaControllerRemote pacienteJpaController;
+	
 	@EJB 
 	private PacienteJpaControllerRemote pacienteDelete;
 
@@ -38,10 +38,6 @@ public class PacienteBean implements Serializable {
 	@EJB
 	ExameJpaControllerRemote exameJpa;
 
-	public PacienteBean() {
-
-	}
-
 	@PostConstruct
 	public void init() {
 		// TODO Auto-generated constructor stub
@@ -50,16 +46,14 @@ public class PacienteBean implements Serializable {
 		listPaciente = pacienteJpaController.findAll();
 		// In case you're updating an existing entity.
 		paciente = new Paciente();
-		
-		
 	}
 
 	public String doManterAgenda() {
-		return "/cadPaciente.xhtml";
+		return "cadPaciente.xhtml";
 	}
 
 	public String goPrincipal() {
-		return "/index.xhtml";
+		return "index.xhtml";
 	}
 
 	public Paciente getPaciente() {
@@ -87,37 +81,22 @@ public class PacienteBean implements Serializable {
 		return "index.xhtml";
 	}
 
-	public String doSomething() {
-
-		System.out.println("DAME");
-		return null;
-	}
-
 	public String deleteAction(Paciente paciente) {
-
-		System.out.println(paciente.getId() + " ==  delete ss");
 		listPaciente.remove(paciente);
 		pacienteController.delete(paciente);
 		return null;
 	}
 	
 	public String deleteExameAction(Exame exame) {
-
-		System.out.println(exame.getId() + " ==  delete ss");
-	
 		exameJpa.delete(exame);
 		listExame.remove(exame);
-		return "/faces/portal/pacienteExames.xhtml";
+		return "pacienteExames.xhtml";
 	}
 	
 	public String examesAction(Paciente paciente) {
-
-		
-	this.paciente = paciente;
-	listExame = exameJpa.findByIdPaciente(paciente.getId());
-	System.out.println("paciente "+paciente.getId());
-	
-		return "/faces/portal/pacienteExames.xhtml";
+		this.paciente = paciente;
+		listExame = exameJpa.findByIdPaciente(paciente.getId());
+		return "pacienteExames.xhtml";
 	}
 	
 	public String update() {
